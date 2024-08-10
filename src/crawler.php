ce925bb7b58fc1790627111010e79c936e76ee1d
Andrew Gerez <andrewgerez18@gmail.com>
2024-08-10 18:03:07 -0300

    feat-100824: dom crawler

--- src/crawler.php
+++ src/crawler.php
@@ -1,8 +1,31 @@
 <?php
 
-use GuzzleHttp\Client;
+require 'vendor/autoload.php';
 
-$client = new Client();
-$response = $client->request('GET', $_ENV['TRACKING_URL']);
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__.'/..');
+$dotenv->load();
+$url = $_ENV['TRACKING_URL'];
+$element = $_ENV['TRACKING_ELEMENT'];
+
+$client = new Client([
+  'curl' => [
+      CURLOPT_SSL_VERIFYPEER => false,
+  ],
+]);
+
+$response = $client->request('GET', $url);
 
 $html = $response->getBody();
+
+$crawler = new Crawler();
+$crawler->addHtmlContent($html);
+
+$elements = $crawler->filter($element);
+
+foreach ($elements as $element) {
+  echo $element->textContent . PHP_EOL;
+}
